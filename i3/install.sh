006c91978aee69b81eb6d69dd6190a3d6f7e842b
briossant <crosson.brieuc@gmail.com>
2023-09-25 19:55:48 +0200

    add i3 + install scripts

---
+++ i3/install.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+cp $SCRIPT_DIR/config ~/.config/i3/config
---
+++ vimForCDev/install.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+cp $SCRIPT_DIR/vimrc ~/.vimrc
